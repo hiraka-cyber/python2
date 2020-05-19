b7773cce1bcd95735f73f71f99cfb5afbe7aac3a
Takeru User <royzgt917@gmail.com>
2020-05-19 22:55:07 +0900

    make it better

--- quiz/settings.py
+++ quiz/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '*m!lnk=!i(85cx6p46jlqd-9tj3l#n4j^=3f=cue5@+9z5y)e9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 
 ALLOWED_HOSTS = []
